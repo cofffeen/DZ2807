a3684eaa86b2de187c6962aaacedc1b3b0afd401
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:30:38 +0700

    Zebra

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zebra
+{
+    internal class Program
+    {
+        public class Zebra
+        {
+            bool isWhite = true;
+            public void DriGetStripe()
+            {
+
+                if (isWhite)
+                {
+                    Console.WriteLine("Полоска белая");
+                }
+                else
+                {
+                    Console.WriteLine("Полоска черная");
+                }
+
+                isWhite = !isWhite;
+                return;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Zebra zebra1 = new Zebra();
+
+            zebra1.DriGetStripe();
+            zebra1.DriGetStripe();
+            zebra1.DriGetStripe();
+
+            Zebra zebra2 = new Zebra();
+            zebra2.DriGetStripe();
+        }
+
+    }
+}
