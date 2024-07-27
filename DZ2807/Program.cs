c1c1f86b4b0efdbf6fa94843efbe8ce256596112
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:10:09 +0700

    Beegeek

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZ
+{
+    internal class Program
+    {
+        public class Beegeek
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
