0de28c16aa57ba25540110675a46441d5d1ff9e8
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:31:49 +0700

    Счётчик 1.0

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveACar
+{
+    internal class Program
+    {
+        public class Counter
+        {
+            int value = 0;
+
+            public void Increment()
+            {
+                value++;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("Текущее значение счетчика = " + value);
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.Show();
+            counter.Increment();
+            counter.Show();
+        }
+
+    }
+}
