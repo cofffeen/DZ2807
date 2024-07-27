fed2a033c269e1b33e2291292a67b9d2a43e4b57
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:29:34 +0700

    Car

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveACar
+{
+    internal class Program
+    {
+        public class Car
+        {
+            public void Drive()
+            {
+                Console.WriteLine("Вын-вын!");
+                Console.WriteLine();
+                return;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Drive();
+        }
+
+    }
+}
