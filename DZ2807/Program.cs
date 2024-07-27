ed420ae02b4b11cdaf5239d72f456b28ada21037
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:18:39 +0700

    Student

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZ
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string FirstName = "Неизвестное имя";
+            public string LastName = "Неизвестная фамилия";
+            public int Age = -1;
+            public double Rating = 0.1;
+        }
+
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.FirstName = "Toyota";
+            s.LastName = "Camry";
+            s.Age = 3;
+            Console.WriteLine(s.FirstName + " " + s.LastName + " " + s.Age);
+        }
+
+    }
+}
