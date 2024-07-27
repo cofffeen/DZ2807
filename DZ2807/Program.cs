786c372a2a8e4cfad7319f8f9ef227ca4b118066
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:24:18 +0700

    Animal

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZ
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public string Name = "Неизвестное имя";
+            public int Age = -1;
+            public int Speed = -1;
+            public int Weight = -1;
+
+        }
+
+        static void Main(string[] args)
+        {
+           
+        }
+
+    }
+}
