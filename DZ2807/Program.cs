6028f928241a649a4ff556ea59370b154e7e10b8
Софья Кожина <kozhinasoff@gmail.com>
2024-07-27 22:13:09 +0700

    School

--- DZ2807/Program.cs
+++ DZ2807/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZ
+{
+    internal class Program
+    {
+        public class School
+        {
+            public string Name = "Неизвестно";
+            public int Number = -1;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
